547f61f6b19646e0705adda816a9d37e7f50ae33
BuildTools <unconfigured@null.spigotmc.org>
2023-08-23 22:58:14 +0800

    login-register API for admin and grantor

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.listen(8081, () => {
     console.log(`Server is running...`);
 })
 
-//register API
+//register API for Applicant
 app.post('/signup', (req, res) => {
     const sql = "INSERT INTO applicant_account (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
     bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
@@ -48,7 +48,7 @@ app.post('/signup', (req, res) => {
     })
 });
 
-//login API
+//login API for Applicant
 app.post('/login', (req, res) => {
     const sql = 'SELECT * FROM applicant_account WHERE email = ?';
     db.query(sql, [req.body.email], (err, data) => {
@@ -58,13 +58,121 @@ app.post('/login', (req, res) => {
                 if (err) return res.json({ Error: "Password compare error" });
                 if (response) {
                     const firstname = data[0].firstname;
-                    const token = jwt.sign({firstname}, "jwt-secret-key", {expiresIn: '1d'});
+                    const token = jwt.sign({ firstname }, "jwt-secret-key", { expiresIn: '1d' });
                     res.cookie('token', token);
+
                     return res.json({ Status: "Success" });
                 } else {
                     return res.json({ Error: "Password not matched" });
                 }
             });
+        } else {
+            return res.json({ Error: "No Email Existed" });
+        }
+    });
+});
+
+// Register API for Grantor
+app.post('/grantor-register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // Extract the image data from the FormData
+        const logoImage = req.body.logo_image[0].buffer;
+
+        const sql = 'INSERT INTO scholarship_providers (organization_name, contact_name, contact_email, phone_number, organization_type, mission_statement, logo_image, website_url, address, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [
+            req.body.organization_name,
+            req.body.contact_name,
+            req.body.contact_email,
+            req.body.phone_number,
+            req.body.organization_type,
+            req.body.mission_statement,
+            logoImage, // Insert the binary image data
+            req.body.website_url,
+            req.body.address,
+            req.body.username,
+            hashedPassword,
+        ];
+
+        db.query(sql, values, (err, result) => {
+            if (err) {
+                return res.json({ Error: 'Error inserting data into database' });
+            }
+            return res.json({ Status: 'Success' });
+        });
+    } catch (error) {
+        return res.json({ Error: 'Error processing registration' });
+    }
+});
+
+//login API for admin
+app.post('/admin-login', (req, res) => {
+    const sql = 'SELECT * FROM admin WHERE email = ?';
+    db.query(sql, [req.body.email], (err, data) => {
+        if (err) return res.json({ Error: "Login error in server" });
+        if (data.length > 0) {
+            if (req.body.password === data[0].password) {
+                return res.json({ Status: "Success" });
+            } else {
+                return res.json({ Error: "Password not matched" });
+            }
+        } else {
+            return res.json({ Error: "No Email Existed" });
+        }
+    });
+});
+
+// //register API for Grantor
+// app.post('/grantor-register2', (req, res) => {
+//     const sql = 'INSERT INTO grantor_account (organization_name, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)';
+//     const values = [
+//         req.body.organization_name,
+//         req.body.firstname,
+//         req.body.lastname,
+//         req.body.email,
+//         req.body.password
+//     ];
+
+//     db.query(sql, values, (err, result) => {
+//         if (err) {
+//             return res.json({ Error: 'Error inserting data into database' });
+//         }
+//         return res.json({ Status: 'Success' });
+//     });
+// });
+
+//register API for Grantor
+app.post('/grantor-register2', (req, res) => {
+    const sql = 'INSERT INTO grantor_account (organization_name, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)';
+    const values = [
+        req.body.organization_name,
+        req.body.firstname,
+        req.body.lastname,
+        req.body.email,
+        req.body.password
+    ];
+
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            return res.json({ Error: 'Error inserting data into database' });
+        }
+        console.log('Registration successful:', result);
+        return res.json({ Status: 'Success' });
+    });
+});
+
+// Grantor login API
+app.post('/grantor-login', (req, res) => {
+    const sql = 'SELECT * FROM grantor_account WHERE email = ?';
+    db.query(sql, [req.body.email], (err, data) => {
+        if (err) return res.json({ Error: "Login error in server" });
+        if (data.length > 0) {
+            if (req.body.password === data[0].password) {
+                return res.json({ Status: "Success" });
+            } else {
+                return res.json({ Error: "Password not matched" });
+            }
         } else {
             return res.json({ Error: "No Email Existed" });
         }
